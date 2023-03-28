28235df573a85e345a3d796a2e8ce1015cdad73d
quilitjustin <quilitjustin8@gmail.com>
2023-03-29 05:27:24 +0800

    Namespace for route service provider

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LoginValidationController;
-use App\Http\Controllers\AnaliticsDashboardController;
-use App\Http\Controllers\LeaderBoardController;
-use App\Http\Controllers\Public\PublicProfileController;
-use App\Http\Controllers\SplashPageController;
-use App\Http\Controllers\ProgrammingLanguageController as ProgLang;
-use App\Http\Controllers\BGMController;
-use App\Http\Controllers\BGImgController;
-use App\Http\Controllers\SoundEffectController;
-use App\Http\Controllers\VisualEffectController;
-use App\Http\Controllers\Public\SplashPageController as PublicSplash;
-use App\Http\Controllers\UsersController;
-use App\Http\Controllers\LiveSearchController;
-use App\Http\Controllers\AnnouncementsController;
-use App\Http\Controllers\BadgesController;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -51,7 +35,7 @@ Route::post('analitics_dashboard', [AnaliticsDashboardController::class, 'index'
 // End
 
 // Public
-Route::get('/', [PublicSplash::class, 'index']);
+Route::get('/', [App\Http\Controllers\Public\SplashPageController::class, 'index']);
 Route::get('/leaderboard', [LeaderBoardController::class, 'index']);
 Route::controller(PublicProfileController::class)->group(function () {
     Route::get('public_profile', 'index')->name('public_profile.index');
